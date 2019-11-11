7478d7d6ad920c491e256904e475970c58a09bd6
AdanWilliam95 <54296292+AdanWilliam95@users.noreply.github.com>
2019-11-10 22:49:30 -0300

    Update TestaPessoa.java

--- src/TestaPessoa.java
+++ src/TestaPessoa.java
@@ -2,7 +2,7 @@
 public class TestaPessoa {
     public static void main(String[] args){
         Pessoa p1 = new Pessoa();
-        p1.nome = "Hermogenes";
+        p1.nome = "Adan";
         p1.idade = 26;
         
         p1.fazAniversario();
